08a93625253a98482f8f7d375587bd8287e3cfcb
zh-plus <11610127@mail.sustech.edu.cn>
2019-12-04 01:57:16 +0800

    Start immediate representation part

---
+++ spl_project3/ir.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 10578 on 12/4/2019.
+//
+
+#include "ir.hpp"
---
+++ spl_project3/ir.hpp
@@ -0,0 +1,8 @@
+//
+// Created by 10578 on 12/4/2019.
+//
+
+#ifndef __IR_HPP__
+#define __IR_HPP__
+
+#endif //__IR_HPP__
